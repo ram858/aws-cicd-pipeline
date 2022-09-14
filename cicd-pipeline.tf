7492a808b6d057c7460cdce0f426059a431efa42
ram <ramadhikari192@gmail.com>
2022-09-14 10:39:18 +0545

    committed

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -9,7 +9,7 @@ resource "aws_codebuild_project" "tf-plan" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform"
+    image                       = "aws/codebuild/standard:6.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
     registry_credential{
